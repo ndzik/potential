aa77af2feecc5037a6ac98e2dc79ccc9b88771d2
Norbert Dzikowski <norbert@perun.network>
2021-02-16 02:34:10 +0100

    [Potential/Test] Correct mock `Boundable` width height order
    Signed-off-by: Norbert Dzikowski <norbert@perun.network>

--- src/Potential/Test/Boundable.hs
+++ src/Potential/Test/Boundable.hs
@@ -3,14 +3,9 @@ module Potential.Test.Boundable where
 
 import           Data.ByteString
 import           Potential.Core
-import           Prelude                        ( ($)
-                                                , (+)
-                                                , Eq(..)
-                                                , Show(..)
-                                                , fromIntegral
-                                                )
+import           Prelude         (Eq (..), Show (..), fromIntegral, ($), (+))
 
 newtype ContentMock = ContentMock ByteString deriving (Eq, Show)
 
 instance Boundable ContentMock where
-  boundsOf (ContentMock s) = Bounds 3 (2 + fromIntegral (length s))
+  boundsOf (ContentMock s) = Bounds (2 + fromIntegral (length s)) 3
